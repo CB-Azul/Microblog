1111f61929fb63e8ca968bec7fc8df5a7b65305b
CB-Azul <christianbognotazul@gmail.com>
2024-01-30 11:36:50 +0800

    creating post and explore users using the app

--- config.py
+++ config.py
@@ -10,3 +10,4 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['christianazul.hybridsolutions@gmail.com']
+    POSTS_PER_PAGE = 3
